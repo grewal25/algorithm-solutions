8e98222482a131ca1ea52759189c85efacc756e6
saran <grewalele@gmail.com>
2024-04-03 18:52:45 -0400

    add code

---
+++ JS/countPairs.js
@@ -0,0 +1,21 @@
+var countPairs = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  let left = 0,
+    right = nums.length - 1;
+  let count = 0;
+
+  while (left < right) {
+    let sum = nums[left] + nums[right];
+    console.log(sum, target);
+    if (sum < target) {
+      count += right - left;
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return count;
+};
+
+console.log(countPairs([1, 2, 3, 4, 5], 7));
---
+++ JS/removeDuplicateNew.js
@@ -0,0 +1,5 @@
+var removeDuplicates = function (nums) {
+  return;
+};
+
+console.log(removeDuplicates([1, 1, 2, 3]));
