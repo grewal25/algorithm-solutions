51caabfe86aa33bcedaf48591eb839399aa66eaa
saran <grewalele@gmail.com>
2024-04-21 21:04:38 -0400

    add solutions

--- JS/summaryRange.js
+++ JS/summaryRange.js
@@ -3,15 +3,12 @@ function summaryRanges(nums) {
 
   for (let i = 0; i < nums.length; i++) {
     let start = nums[i];
-
     while (i + 1 < nums.length && nums[i + 1] - nums[i] === 1) {
       i++;
     }
-
     let end = nums[i];
     ranges.push(end === start ? start.toString() : start + "->" + end);
   }
-
   return ranges;
 }
 
