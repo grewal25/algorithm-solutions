e9212b17a168de3710c99135822e52b44b866365
saran <grewalele@gmail.com>
2024-04-27 20:00:27 -0400

    add solutions

--- JS/maxProfit.js
+++ JS/maxProfit.js
@@ -17,28 +17,5 @@ function maxProfit(prices) {
   return maxProfit;
 }
 
-// Example usage:
 const prices = [7, 1, 5, 3, 6, 4];
-console.log(maxProfit(prices)); // Output should be 5
-
-// function maxProfit(prices) {
-//   let minPrice = Infinity;
-//   let maxProfit = 0;
-
-//   for (let price of prices) {
-//     console.log("1-->", price, minPrice);
-//     if (price < minPrice) {
-//       minPrice = price;
-//       console.log("2", price, minPrice);
-//       console.log("3", price - minPrice > maxProfit);
-//     } else if (price - minPrice > maxProfit) {
-//       maxProfit = price - minPrice;
-//     }
-//   }
-
-//   return maxProfit;
-// }
-
-// // Example usage:
-// const prices = [7, 1, 5, 3, 6, 4];
-// console.log(maxProfit(prices)); // Output should be 5
+console.log(maxProfit(prices));
